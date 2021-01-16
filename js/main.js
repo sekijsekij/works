6e634e38216c388d2e78372171e9e567e0e81c4c
sekijsekij <sekij.sekiyajunichi@anahippie ~ % cd ls -a | grep .git.git.gitconfig.gitflow_export.gitignore_globalsekiyajunichi@anahippie ~ % rm -rf .gitsekiyajunichi@anahippie ~ % sekij.lust.for.life1986@gmail.com>
2021-01-16 10:00:06 +0900

    js追加

--- js/main.js
+++ js/main.js
@@ -1,25 +1,36 @@
-$(function() {
-  $(".nav-button").on("click", function() {
-    if ($(this).hasClass("active")) {
-      $(this).removeClass("active");
-      $(".nav-wrap")
-        .addClass("close")
-        .removeClass("open");
-    } else {
-      $(this).addClass("active");
-      $(".nav-wrap")
-        .addClass("open")
-        .removeClass("close");
+$(function(){
+  var imgHeight = $('.kv-wrapper').outerHeight(); 
+  var bgHeight = $('.fv').outerHeight(); 
+  
+
+  $(".burger-btn").on('click',function(){
+    if( $(window).scrollTop() < imgHeight -50){
+      
+      $('.bar').toggleClass('cross'); 
+      $('.header-nav').toggleClass('open'); 
+      $('.burger-musk').fadeToggle(300); 
+      $('body').toggleClass('noscroll'); 
+    }else{
+      $(this).toggleClass('black'); 
+      $('.bar').toggleClass('cross');
+      $('.header-nav').toggleClass('open');
+      $('.burger-musk').fadeToggle(300);
+      $('body').toggleClass('noscroll');
     }
   });
+
+
+  $(window).on('load scroll', function(){
+     if ($(window).scrollTop() < imgHeight -50) {
+       $('.site-title').removeClass('black');
+     }else {
+       $('.site-title').addClass('black');
+     }
+
+     if ($(window).scrollTop() < bgHeight -50) {
+       $('.burger-btn').removeClass('black');
+     }else {
+       $('.burger-btn').addClass('black');
+     }
+  });
 });
-let swipeOption = {
-  loop: true,
-  effect: 'fade',
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: false,
-  },
-  speed: 2000,
-}
-new Swiper('.swiper-container', swipeOption);
